2de12f4d7504d760ed331a86c8187a588ea3ddde
Daniel McKemie <daniel.mckemie@gmail.com>
2018-11-01 23:35:56 -0400

    implemented left to right delay

--- sound.js
+++ sound.js
@@ -1,18 +1,25 @@
 // Components go from bottom up...
 // (Source --> Effects --> Output)
 
-let feedbackDelay = new Tone.FeedbackDelay(0.133, 0.01).toMaster()
+// TO DO:
+// FIGURE OUT HOW TO DELAY INTO RIGHT CHANNEL
+
+let rightChannel = new Tone.Panner(1).connect(machineReverb).toMaster()
+let tapeDelay = new Tone.Delay(8, 8).connect(rightChannel)
+let leftChannel = new Tone.Panner(-1).connect(tapeDelay).toMaster()
+let machineReverb = new Tone.FeedbackDelay(0.133, 0.01).connect(leftChannel, tapeDelay)
+
 
 // Oscillator amplitudes...In order for these to 
 // read at an accurate scale of -12 to 12, the 
 // internal computer volume must be set to 50%
 const oscAmp1 = new Tone.Volume({
   volume: -12,
-}).toMaster()
+}).connect(leftChannel)
 
 const oscAmp2 = new Tone.Volume({
   volume: -12,
-}).toMaster()
+}).connect(leftChannel)
 
 // Oscillators
 const osc1 = new Tone.Oscillator({
@@ -20,14 +27,14 @@ const osc1 = new Tone.Oscillator({
 	modulationIndex: 0,
 	modulationType: "sine",
 	harmonicity: 0
-}).chain(feedbackDelay, oscAmp1).start()
+}).chain(machineReverb, oscAmp1).start()
 
 const osc2 = new Tone.Oscillator({
   frequency: 0,
   modulationIndex: 0,
   modulationType: "sine",
   harmonicity: 0
-}).chain(feedbackDelay, oscAmp2).start()
+}).chain(machineReverb, oscAmp2).start()
 
 
 // Bias frequency to emulate tape machine
@@ -36,5 +43,5 @@ const biasFreq = new Tone.Oscillator({
   modulationIndex: 0,
   modulationType: "sine",
   harmonicity: 0
-}).chain(feedbackDelay).toMaster()
+}).chain(machineReverb).toMaster()
 .start()
