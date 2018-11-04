4b3026598171b48adbd7148eebdf742c42c1989f
Daniel McKemie <daniel.mckemie@gmail.com>
2018-11-04 13:17:44 -0500

    back to initial design

--- public/interface.js
+++ public/interface.js
@@ -35,83 +35,104 @@ inputSwitch.on('change', function(x) {
 
 
 // Oscillator dials
-let dial1 = $(function() {
-  $(".dial1").knob({
-    min: 22000,
-    max: 34265,
-    'change': function(x) {
-      osc1.frequency.value = x
-    }
-  });
+const dial1 = new Nexus.Dial('#dial1', {
+  'size': [150, 150],
+  'interaction': 'radial', // "radial", "vertical", or "horizontal"
+  'mode': 'relative', // "absolute" or "relative"
+  'min': 22000,
+  'max': 34265,
+  'step': 0,
+  'value': 0
 })
 
-let dial2 = $(function() {  
-  $(".dial2").knob({
-    min: 22000,
-    max: 34700,
-    'change': function(x) {
-      osc2.frequency.value = x
-    }
-  });
+dial1.on('change', function(x) {
+  osc1.frequency.value = x
+
 })
 
+const dial2 = new Nexus.Dial('#dial2', {
+  'size': [150, 150],
+  'mode': 'relative', // 'relative' or 'absolute'
+  'min': 22000,
+  'max': 34700,
+  'step': 0,
+  'value': 0
+})
 
-// const dial1 = new Nexus.Dial('#dial1', {
-//   'size': [150, 150],
-//   'interaction': 'radial', // "radial", "vertical", or "horizontal"
-//   'mode': 'relative', // "absolute" or "relative"
-//   'min': 22000,
-//   'max': 34265,
-//   'step': 0,
-//   'value': 0
-// })
+dial2.on('change', function(x) {
+  osc2.frequency.value = x
+})
 
-// dial1.on('change', function(x) {
-//   osc1.frequency.value = x
-
-// })
-
-// const dial2 = new Nexus.Dial('#dial2', {
-//   'size': [150, 150],
-//   'mode': 'relative', // 'relative' or 'absolute'
-//   'min': 22000,
-//   'max': 34700,
-//   'step': 0,
-//   'value': 0
-// })
-
-// dial2.on('change', function(x) {
-//   osc2.frequency.value = x
-// })
 
 // Oscillator amplitude controls
-const fader1 = document.getElementById("slider1");
-fader1.oninput = function() {
-  oscAmp1.volume.value = this.value
-}
+const fader1 = new Nexus.Slider('#slider1', {
+  'size': [150, 20],
+  'mode': 'relative', // 'relative' or 'absolute'
+  'min': -12,
+  'max': 12,
+  'step': 0,
+  'value': -12
+});
 
-const fader2 = document.getElementById("slider2");
-fader2.oninput = function() {
-  oscAmp2.volume.value = this.value
-}
+fader1.on('change', function(x) {
+  oscAmp1.volume.value = x
+})
+
+const fader2 = new Nexus.Slider('#slider2', {
+  'size': [150, 20],
+  'mode': 'relative', // 'relative' or 'absolute'
+  'min': -12,
+  'max': 12,
+  'step': 0,
+  'value': -12
+});
+
+fader2.on('change', function(x) {
+  oscAmp2.volume.value = x
+})
 
 
 // Feedback depth controls
 
 // For input into L from delayed R
-const fader3 = document.getElementById("slider3");
-fader3.oninput = function() {
-  tapeDelayL2Amp.volume.value = this.value
-}
+const fader3 = new Nexus.Slider('#slider3', {
+  'size': [150, 20],
+  'mode': 'relative', // 'relative' or 'absolute'
+  'min': -24,
+  'max': 0,
+  'step': 0,
+  'value': -24
+});
 
-const fader4 = document.getElementById("slider4");
-fader4.oninput = function() {
-  tapeDelayRAmp.volume.value = this.value
-}
+fader3.on('change', function(x) {
+  tapeDelayL2Amp.volume.value = x
+})
+
+// For L channel input coupled back
+const fader4 = new Nexus.Slider('#slider4', {
+  'size': [150, 20],
+  'mode': 'relative', // 'relative' or 'absolute'
+  'min': -24,
+  'max': 0,
+  'step': 0,
+  'value': -24
+});
+
+fader4.on('change', function(x) {
+  tapeDelayRAmp.volume.value = x
+})
 
 
 // Input Gain
-const fader5 = document.getElementById("slider5");
-fader5.oninput = function() {
-  userAmp.volume.value = this.value
-}
+const fader5 = new Nexus.Slider('#slider5', {
+  'size': [150, 20],
+  'mode': 'relative', // 'relative' or 'absolute'
+  'min': -24,
+  'max': 12,
+  'step': 0,
+  'value': -24
+});
+
+fader5.on('change', function(x) {
+  userAmp.volume.value = x
+})
